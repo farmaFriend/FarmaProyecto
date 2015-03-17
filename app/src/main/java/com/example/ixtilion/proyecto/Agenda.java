e52575644a595971866f305309eb6867a6ce24c5
Victor <ixtilion@gmail.com>
2015-03-17 16:16:35 +0100

    Base de datos en agenda

--- app/src/main/java/com/example/ixtilion/proyecto/Agenda.java
+++ app/src/main/java/com/example/ixtilion/proyecto/Agenda.java
@@ -30,28 +30,28 @@ public class Agenda extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 
-        //dbOp = new DatabaseOperations(container.getContext());
-        //cursor = dbOp.cargarCursorContactos();
+        dbOp = new DatabaseOperations(container.getContext());
+        cursor = dbOp.cargarCursorContactos();
 
         //Aqui se haria una query a la base de datos, se recorreria el puntero e iriamos construyendo y añadiendo contactos al array
-        Contacto c1 = new Contacto("Pepe", "23424");
-        Contacto c2 = new Contacto( "Juan", "3356");
+       // Contacto c1 = new Contacto("Pepe", "23424");
+        // Contacto c2 = new Contacto( "Juan", "3356");
 
         final ArrayList<Contacto> contacts = new ArrayList<Contacto>();
 
-        contacts.add(c1);
-        contacts.add(c2);
+        //contacts.add(c1);
+        //contacts.add(c2);
 
-        //if(cursor.moveToFirst()){
-          //  do{
-            //    String nombre = cursor.getString(0);
-              //  String telefono = cursor.getString(1);
+        if(cursor.moveToFirst()){
+            do{
+                String nombre = cursor.getString(0);
+                String telefono = cursor.getString(1);
 
 
-                //contacts.add(new Contacto(nombre, telefono));
+                contacts.add(new Contacto(nombre, telefono));
 
-            //}while (cursor.moveToNext());
-        //}
+            }while (cursor.moveToNext());
+        }
 
         View view = inflater.inflate(R.layout.lay_agenda, container, false);
         ListView list = (ListView)view.findViewById(R.id.listView);
