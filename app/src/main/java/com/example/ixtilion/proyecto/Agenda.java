192fc60e506a722b5372210e16e9d3a7021fd2e1
Victor <ixtilion@gmail.com>
2015-03-19 16:48:38 +0100

    Mandar desde agenda a nuevo contacto

--- app/src/main/java/com/example/ixtilion/proyecto/Agenda.java
+++ app/src/main/java/com/example/ixtilion/proyecto/Agenda.java
@@ -1,6 +1,7 @@
 package com.example.ixtilion.proyecto;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -11,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -58,6 +60,22 @@ public class Agenda extends Fragment{
         ListView list = (ListView)view.findViewById(R.id.listView);
         final CustomArrayAdapter adapter = new CustomArrayAdapter(view.getContext(),contacts);
         list.setAdapter(adapter);
+        View imageButton = (ImageButton) view.findViewById(R.id.anadirContacto);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                FragmentManager fm = getFragmentManager();
+                Fragment fragmento = new Agenda();
+                fm.beginTransaction()
+                        .replace(R.id.container, new Anadir_contacto() )
+                        .commit();
+
+            }
+
+        });
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
