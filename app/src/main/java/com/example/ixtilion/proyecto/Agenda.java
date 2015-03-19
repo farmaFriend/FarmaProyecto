5536d42bc702f5cf96097d87063a185a885ef0bd
Cristina <crlaenci@unirioja.es>
2015-03-19 17:32:01 +0100

    No poder añadir 2 contactos con el mismo numero de telefono

--- app/src/main/java/com/example/ixtilion/proyecto/Agenda.java
+++ app/src/main/java/com/example/ixtilion/proyecto/Agenda.java
@@ -35,14 +35,7 @@ public class Agenda extends Fragment{
         dbOp = new DatabaseOperations(container.getContext());
         cursor = dbOp.cargarCursorContactos();
 
-        //Aqui se haria una query a la base de datos, se recorreria el puntero e iriamos construyendo y añadiendo contactos al array
-       // Contacto c1 = new Contacto("Pepe", "23424");
-        // Contacto c2 = new Contacto( "Juan", "3356");
-
         final ArrayList<Contacto> contacts = new ArrayList<Contacto>();
-
-        //contacts.add(c1);
-        //contacts.add(c2);
 
         if(cursor.moveToFirst()){
             do{
--- app/src/main/java/com/example/ixtilion/proyecto/Anadir_contacto.java
+++ app/src/main/java/com/example/ixtilion/proyecto/Anadir_contacto.java
@@ -5,6 +5,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -16,6 +17,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 
 /**
  * Created by USUARIO on 12/03/2015.
@@ -25,6 +28,8 @@ public class Anadir_contacto extends Fragment {
     String id, nombre, telf;
     Button anadir;
     Context c;
+    private DatabaseOperations dbOp;
+    Cursor cursor;
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -67,20 +72,53 @@ public class Anadir_contacto extends Fragment {
                             cv.put(TableData.TableInfo.COLUMN_NAME_TELEFEONO, telf);
 
 
-                            db.insert(TableData.TableInfo.TABLE_NAME_AGENDA, null, cv);
-                            Log.d("Operaciones bases de datos", "Insertada una fila");
+                            //Mirar si en la base de datos exite un contacto con el mismo telefono
 
-                            db.close();
+                            dbOp = new DatabaseOperations(c);
+                            cursor = dbOp.cargarCursorContactos();
 
-                            Toast.makeText(c, "Se ha añadido el contacto correctamente", Toast.LENGTH_LONG).show();
+                            final ArrayList<Contacto> contacts = new ArrayList<Contacto>();
+
+                            if(cursor.moveToFirst()){
+                                do{
+
+                                    String nombre = cursor.getString(0);
+                                    String telefono = cursor.getString(1);
 
 
-                            //CODIGO QUE MANDA A VISTA AGENDA
-                            FragmentManager fm = getFragmentManager();
-                            Fragment fragmento = new Agenda();
-                            fm.beginTransaction()
-                                    .replace(R.id.container, new Agenda() )
-                                    .commit();
+                                    contacts.add(new Contacto(nombre, telefono));
+
+                                }while (cursor.moveToNext());
+                            }
+
+                            int i=0;
+                            boolean existe=false;
+                            while(i<contacts.size()&& existe==false) {
+                                if(telf.compareTo(contacts.get(i).getPhone())==0){
+                                    Toast.makeText(c, "Ya existe un contacto con ese número de teléfono", Toast.LENGTH_LONG).show();
+                                    existe=true;
+                                }
+                                i++;
+                            }
+                            if(existe==false){
+                                db.insert(TableData.TableInfo.TABLE_NAME_AGENDA, null, cv);
+                                Log.d("Operaciones bases de datos", "Insertada una fila");
+
+                                db.close();
+
+                                Toast.makeText(c, "Se ha añadido el contacto correctamente", Toast.LENGTH_LONG).show();
+
+
+                                //CODIGO QUE MANDA A VISTA AGENDA
+                                FragmentManager fm = getFragmentManager();
+                                Fragment fragmento = new Agenda();
+                                fm.beginTransaction()
+                                        .replace(R.id.container, new Agenda() )
+                                        .commit();
+                            }
+
+
+
                         }
                     }
                     NOMBRE.setText("");
